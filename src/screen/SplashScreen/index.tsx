8ec567ade9f45966d904af255f14d185d6bdb794
andichou <andichou09@gmail.com>
2021-09-13 22:14:07 +0700

    implement splash screen animation

---
+++ src/screen/SplashScreen/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import LottieView from 'lottie-react-native';
+import { Modal } from 'react-native';
+
+interface Props {}
+export const SplashScreen: React.FC<Props> = (props) => {
+  const [hasAnimationPlayedOnce, setHasAnimationPlayedOnce] = React.useState(false)
+
+  const handleAnimationFinish = () => {
+    setHasAnimationPlayedOnce(true)
+  }
+
+  const isModalVisible = !(hasAnimationPlayedOnce)
+
+  return (
+      <Modal visible={isModalVisible} animationType="fade">
+        <LottieView source={require('../../assets/welcome.json')}
+                    autoPlay
+                    loop={false}
+                    onAnimationFinish={handleAnimationFinish} />
+      </Modal>
+  );
+};
